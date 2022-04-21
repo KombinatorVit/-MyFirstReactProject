5af4a16abc0941b70e86d4253c1e88db3f92ddaf
Vit Onischenko <slonik0775@gmail.com>
2022-04-21 06:37:26 +0300

    Working with Forms, Managed and Unmanaged Components

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -1,6 +1,24 @@
+import { Component } from 'react';
+
 import './employers-add-form.css';
 
-const EmpoyersAddForm = () => {
+class EmpoyersAddForm extends Component{
+constructor(props){
+    super(props);
+    this.state = {
+        name: '',
+        salary: 0
+    }
+}
+
+onValueChange = (e) => {
+    this.setState({
+        [e.target.name] : e.target.value
+    })
+}
+
+
+    render() {
     return (
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
@@ -8,10 +26,14 @@ const EmpoyersAddForm = () => {
                 className="add-form d-flex">
                 <input type="text"
                     className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
+                    placeholder="Как его зовут?"
+                    name="name"
+                    onChange={this.onValueChange} />
                 <input type="number"
                     className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+                    placeholder="З/П в $?" 
+                    name="salary"
+                    onChange={this.onValueChange}/>
 
                 <button type="submit"
                         className="btn btn-outline-light">Добавить</button>
@@ -19,6 +41,7 @@ const EmpoyersAddForm = () => {
         </div>
     )
 
+}
 }
 
 export default EmpoyersAddForm;
